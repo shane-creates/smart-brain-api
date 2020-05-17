5e7d2aa625e9dbf78b2fb06c4893cafb074f4c41
Shane Hall <Shane@ashleys-mbp.lan>
2020-05-17 14:43:59 -0700

    fixing errors

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host : 'postgresql-triangular-61486',
-        user : '',
-        password : '',
-        database : 'smart-brain'
+        host : process.env.DATABASE_URL,
+        ssl: true,
     }
 
 });
